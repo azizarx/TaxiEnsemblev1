7b86d7171277b0a24da664e008a43b301e192b21
Elhaj Elhaithem <101217966+haithemelhadj@users.noreply.github.com>
2023-07-09 14:02:52 +0100

    haithem commit
    buttons: breaks move left and right, get paid, smoke, stress level and code optimizations

--- Assets/Scripts/TaxiController.cs
+++ Assets/Scripts/TaxiController.cs
@@ -1,25 +1,46 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TaxiController : MonoBehaviour
 {
+    public HoldButton HoldButton;
     public Animator animator;
     public Transform[] path; // The path the car will follow
-    public float speed = 10f; // The speed of the car
-    
+    public float MaxSpeed = 10f; // The MaxSpeed of the car
+    public float Speed = 10f; // The MaxSpeed of the car
+    public float Acceleration = 1f;
+    public float Deceleration = 1f;
+    Vector3 forwardDirection;
     private void start()
     {
         animator = GetComponent<Animator>();
+        //forwardDirection = transform.forward; // Get the forward direction of the car
     }
     private void Update()
     {
         MoveForward();
-        ChangePath();
+        Accelerate();
+        movement();
+        //ChangePath();
+    }
+    void Accelerate()
+    {
+        if (HoldButton.isHeld)
+        {
+            Speed = Mathf.MoveTowards(Speed, 0f, Deceleration * Time.deltaTime);
+            //Speed -= Deceleration * Time.deltaTime;
+        }
+        else if (Speed < MaxSpeed) 
+        {
+            Speed = Mathf.MoveTowards(Speed, MaxSpeed, Acceleration * Time.deltaTime);
+        }
     }
 
     private void MoveForward()
     {
-        Vector3 forwardDirection = transform.forward; // Get the forward direction of the car
-        Vector3 movement = forwardDirection * speed * Time.deltaTime; // Calculate the movement vector
+        Vector3 movement = transform.forward * Speed * Time.deltaTime; // Calculate the movement vector
 
         transform.position += movement; // Move the car forward
     }
@@ -61,6 +82,48 @@ public class TaxiController : MonoBehaviour
                 return;
             }
         }
+    }
+
+
+
+    //public float MaxSpeed;
+    public int curPos = 0;
+    //public float horizontal;
+    void movement()//the movement of the vecle 
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            //horizontal = -1;
+            if (curPos > -1)  { curPos--; }
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //horizontal = 1;
+            if (curPos < 1)  { curPos++; }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(path[curPos+1].position.x, transform.position.y, transform.position.z), Speed * Time.deltaTime);
+
+        //else horizontal = 0;
+        /*
+        if (curPos != 0 && horizontal != 0 && Math.Sign(curPos) != Math.Sign(horizontal)) 
+        {
+            curPos = 0;
+        }
+        else if (curPos == 0 && horizontal != 0)
+        {
+            curPos = horizontal * 4;
+            curPos= path[0].position.x;
+        }
+        */
+    }
+
+    public void RightButton()
+    {
+        if (curPos < 1) { curPos++; }
+    }
+    public void LeftButton()
+    {
+        if (curPos > -1) { curPos--; }
     }
 }
 
